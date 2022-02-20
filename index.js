e9b512d1750265eac6471189f8c4bafae0972bea
Bilal Raza <smbraza97@gmail.com>
2022-02-21 04:59:25 +0500

    added cool ASCII art as a way to show that the app has activated

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const consoleTable = require("console.table");
+
+console.log(`
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+████████╗███████╗████████╗ █████╗ 
+╚══██╔══╝██╔════╝╚══██╔══╝██╔══██╗
+   ██║   █████╗     ██║   ███████║
+   ██║   ██╔══╝     ██║   ██╔══██║
+   ██║   ███████╗   ██║   ██║  ██║
+   ╚═╝   ╚══════╝   ╚═╝   ╚═╝  ╚═╝
+    The Employee-Tracking App
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+`);
