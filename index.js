2a918ed8e5def2b65d85cfb665595a954fd40946
Bilal Raza <smbraza97@gmail.com>
2022-02-23 03:45:24 +0500

    added bonus functions to program. it is now fully finished.

--- index.js
+++ index.js
@@ -43,6 +43,13 @@ let initPrompt = () => {
         "Add a Role",
         "Add an Employee",
         "Update an Employee's Role",
+        "Update an Employee's Manager",
+        "View Employees By Manager",
+        "View Employees By Department",
+        "Delete a Department",
+        "Delete a Role",
+        "Delete an Employee",
+        "View Total Utilized Budget for a Department",
         "Exit"
       ],
     },
@@ -71,6 +78,27 @@ let initPrompt = () => {
         case "Update an Employee's Role":
           updateEmployeeRole();
           break;
+        case "Update an Employee's Manager":
+          updateEmployeeManager();
+          break;
+        case "View Employees By Manager":
+          viewEmployeesByManager();
+          break;
+        case "View Employees By Department":
+          viewEmployeesByDepartment();
+          break;
+        case "Delete a Department":
+          deleteDepartment();
+          break;
+        case "Delete a Role":
+          deleteRole();
+          break;
+        case "Delete an Employee":
+          deleteEmployee();
+          break;
+        case "View Total Utilized Budget for a Department":
+          viewTotalBudget();
+          break;
         // If the user wishes to exit the application
         case "Exit":
           console.log("Goodbye!");
@@ -91,7 +119,9 @@ let viewAllDepartments = () => {
 
 // Function for viewing a table showing roles with job title, role id, the department that role belongs to, and the salary for that role
 let viewAllRoles = () => {
-  db.query(`SELECT role.title AS "Role", role.id AS "Role Id", department.name AS "Department", role.salary AS "Salary ($)" FROM role JOIN department ON role.department_id = department.id;`, (err, result) => {
+  db.query(`SELECT role.title AS "Role", role.id AS "Role Id", 
+  department.name AS "Department", role.salary AS "Salary ($)" 
+  FROM role JOIN department ON role.department_id = department.id;`, (err, result) => {
     if (err) throw err;
     console.table(result);
     initPrompt();
@@ -100,7 +130,12 @@ let viewAllRoles = () => {
 
 // Function for viewing a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 let viewAllEmployees = () => {
-  db.query(`SELECT employee.first_name AS "First Name", employee.last_name AS "Last Name", role.title as "Job Title", department.name AS "Department", role.salary AS "Salary ($)", CONCAT(e.first_name, ' ' , e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department ON department.id = role.department_id LEFT JOIN employee e ON employee.manager_id = e.id;`, (err, result) => {
+  db.query(`SELECT e.first_name AS "First Name", e.last_name AS "Last Name", 
+  r.title AS "Job Title", d.name AS "Department", r.salary AS "Salary ($)",
+  CONCAT(m.first_name, " ", m.last_name) AS "Manager" 
+  FROM employee AS e LEFT JOIN role AS r ON e.role_id = r.id 
+  LEFT JOIN department AS d ON r.department_id = d.id 
+  LEFT JOIN employee AS m ON e.manager_id = m.id;`, (err, result) => {
     if (err) throw err;
     console.table(result);
     initPrompt();
@@ -167,10 +202,10 @@ let addRole = () => {
       .then((answer) => {
         // Finally, make a query statement that inserts new role into table using name of new role and department id
         db.query(`INSERT INTO role (title, salary, department_id) VALUES (?)`, [[answer.newRole, answer.newSalary, answer.department]], (err, result) => {
-            if (err) throw err;
-            console.log(`Your new role, ${answer.newRole} has been added!`);
-            initPrompt();
-          });
+          if (err) throw err;
+          console.log(`Your new role, ${answer.newRole} has been added!`);
+          initPrompt();
+        });
       });
   });
 }
@@ -240,7 +275,7 @@ let addEmployee = () => {
           let managerId = answer.managerName !== 0 ? answer.managerName : null;
 
           // Finally make a query statement to insert new row into employee table
-          db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?)`, [[answer.firstName, answer.lastName, answer.roleId, managerId]], (err, response) => {
+          db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?)`, [[answer.firstName, answer.lastName, answer.roleId, managerId]], (err, result) => {
             if (err) throw err;
             console.log(`Added ${answer.firstName} ${answer.lastName} to the database`);
             initPrompt();
@@ -307,34 +342,264 @@ let updateEmployeeRole = () => {
   });
 }
 
-// Bonus Functions:
+// Bonus Functions. These functions use the same coding pattern of previous ones, where a SELECT * FROM statement is used to retrieve a table, then another query is called with the statement used to view/update/delete rows
 
 let updateEmployeeManager = () => {
+  db.query("SELECT * FROM employee", (err, result) => {
+    if (err) throw err;
 
+    let employeeChoices = [];
+
+    result.forEach(({ first_name, last_name, id }) => {
+      employeeChoices.push({
+        name: first_name + " " + last_name,
+        value: id
+      });
+    });
+
+    let managerChoices = [{
+      name: 'None',
+      value: 0
+    }];
+
+    result.forEach(({ first_name, last_name, id }) => {
+      managerChoices.push({
+        name: first_name + " " + last_name,
+        value: id
+      });
+    });
+
+    inquirer.prompt([
+      {
+        type: "list",
+        message: "Select employee whose manager you wish to change:",
+        name: "employeeId",
+        choices: employeeChoices
+      },
+      {
+        type: "list",
+        message: "Who is the employee's new manager?",
+        name: "managerId",
+        choices: managerChoices
+      }
+    ])
+      .then((answer) => {
+
+        let manager_id = answer.managerId !== 0 ? answer.managerId : null;
+
+        db.query(`UPDATE employee SET ? WHERE id = ?;`, [{ manager_id: manager_id }, answer.employeeId], (err, result) => {
+          if (err) throw err;
+          console.log("The employee now has a new manager!");
+          initPrompt();
+        });
+      });
+  });
 }
 
-let viewAllEmployeesByManager = () => {
+let viewEmployeesByManager = () => {
+  db.query("SELECT * FROM employee", (err, result) => {
+    if (err) throw err;
 
+    let managerChoices = [{
+      name: 'None',
+      value: 0
+    }];
+
+    result.forEach(({ first_name, last_name, id }) => {
+      managerChoices.push({
+        name: first_name + " " + last_name,
+        value: id
+      });
+    });
+
+    inquirer.prompt([
+      {
+        type: "list",
+        message: "Which manager's employees would you like to view?",
+        name: "managerId",
+        choices: managerChoices
+      }
+    ])
+      .then((answer) => {
+
+        let manager_id = answer.managerId !== 0 ? answer.managerId : null;
+        let whereStatement = answer.manager_id !== 0 ? "= ?" : "IS NULL";
+
+        db.query(`SELECT e.first_name AS "First Name", e.last_name AS "Last Name", 
+          r.title AS "Job Title", d.name AS "Department", r.salary AS "Salary ($)",
+          CONCAT(m.first_name, " ", m.last_name) AS "Manager" 
+          FROM employee AS e LEFT JOIN role AS r ON e.role_id = r.id 
+          LEFT JOIN department AS d ON r.department_id = d.id 
+          LEFT JOIN employee AS m ON e.manager_id = m.id 
+          WHERE e.manager_id ${whereStatement};`, [answer.managerId], (err, result) => {
+          if (err) throw err;
+          console.table(result);
+          initPrompt();
+        });
+      });
+  });
 }
 
-let viewAllEmployeesByDepartment = () => {
+let viewEmployeesByDepartment = () => {
+  db.query('SELECT * FROM department', (err, result) => {
+    if (err) throw err;
 
+    let departmentChoices = []
+
+    result.forEach(department => {
+      departmentChoices.push({
+        name: department.name,
+        value: department.id
+      });
+    });
+
+    inquirer.prompt([
+      {
+        type: "list",
+        message: "Which department's employees would you like to view?",
+        name: "departmentId",
+        choices: departmentChoices
+      },
+    ])
+      .then((answer) => {
+        db.query(`SELECT e.first_name AS "First Name", e.last_name AS "Last Name", 
+        r.title AS "Job Title", d.name AS "Department", r.salary AS "Salary ($)",
+        CONCAT(m.first_name, " ", m.last_name) AS "Manager" 
+        FROM employee AS e LEFT JOIN role AS r ON e.role_id = r.id 
+        LEFT JOIN department AS d ON r.department_id = d.id 
+        LEFT JOIN employee AS m ON e.manager_id = m.id 
+        WHERE d.id = ?;`, [answer.departmentId], (err, result) => {
+          if (err) throw err;
+          console.table(result);
+          initPrompt();
+        });
+      });
+  });
 }
 
 let deleteDepartment = () => {
+  db.query('SELECT * FROM department', (err, result) => {
+    if (err) throw err;
 
+    let departmentChoices = []
+
+    result.forEach(department => {
+      departmentChoices.push({
+        name: department.name,
+        value: department.id
+      });
+    });
+
+    inquirer.prompt([
+      {
+        type: "list",
+        message: "Which department do you want to delete?",
+        name: "departmentId",
+        choices: departmentChoices
+      },
+    ])
+      .then((answer) => {
+        db.query(`DELETE FROM department WHERE id = ?;`, [answer.departmentId], (err, result) => {
+          if (err) throw err;
+          console.log(`The department has been successfully deleted.`);
+          initPrompt();
+        });
+      });
+  });
 }
 
 let deleteRole = () => {
+  db.query("SELECT * FROM role", (err, result) => {
+    if (err) throw err;
 
+    let roleChoices = [];
+    result.forEach(({ title, id }) => {
+      roleChoices.push({
+        name: title,
+        value: id
+      });
+    });
+
+    inquirer.prompt([
+      {
+        type: "list",
+        name: "roleId",
+        message: "Which role would you like to delete?",
+        choices: roleChoices
+      }
+    ])
+      .then((answer) => {
+        db.query(`DELETE FROM role WHERE id = ?`, [answer.roleId], (err, response) => {
+          if (err) throw err;
+          console.log(`The role has been successfully deleted.`);
+          initPrompt();
+        });
+      });
+  });
 }
 
 let deleteEmployee = () => {
+  db.query("SELECT * FROM employee", (err, result) => {
+    if (err) throw err;
 
+    let employeeChoices = [];
+
+    result.forEach(({ first_name, last_name, id }) => {
+      employeeChoices.push({
+        name: first_name + " " + last_name,
+        value: id
+      });
+    });
+
+    inquirer.prompt([
+      {
+        type: "list",
+        message: "Which employee would you like to delete?",
+        name: "employeeId",
+        choices: employeeChoices
+      }
+    ])
+      .then((answer) => {
+        db.query(`DELETE FROM employee WHERE id = ?;`, [answer.employeeId], (err, res) => {
+          if (err) throw err;
+          console.log("The employee has been deleted!");
+          initPrompt();
+        });
+      });
+  });
 }
 
 let viewTotalBudget = () => {
+  db.query('SELECT * FROM department', (err, result) => {
+    if (err) throw err;
 
+    let departmentChoices = []
+
+    result.forEach(department => {
+      departmentChoices.push({
+        name: department.name,
+        value: department.id
+      });
+    });
+
+    inquirer.prompt([
+      {
+        type: "list",
+        message: "Which department's budget do you want to view?",
+        name: "departmentId",
+        choices: departmentChoices
+      },
+    ])
+      .then((answer) => {
+        db.query(`SELECT d.name AS "Department", SUM(salary) AS "Total Utilized Budget ($)" 
+        FROM employee AS e LEFT JOIN role AS r ON e.role_id = r.id 
+        LEFT JOIN department AS d ON r.department_id = d.id WHERE d.id = ?`, [answer.departmentId], (err, result) => {
+          if (err) throw err;
+          console.table(result);
+          initPrompt();
+        });
+      });
+  });
 }
 
 // Start program:
